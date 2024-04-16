bcab90b3735b2f8578e2ac66f94dd1ba8be8e65a
AndresHaug <andres.haug.s25@gmail.com>
2024-04-15 20:34:11 -0600

    login
    Login

---
+++ santory5/santory5/src/main/java/com/santory5/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.santory5.dao;
+
+import com.santory5.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
